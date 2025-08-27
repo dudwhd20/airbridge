33dafc49867764151d218c2185fab16ab30e01a2
soyj <dudwhd20@naver.com>
2025-08-27 23:07:05 +0900

    feat : mockito 추가 & spring boot test추가

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.0") // or mockito
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
 }
 
 tasks.withType<Test> {
